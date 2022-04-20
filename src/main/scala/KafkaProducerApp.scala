68a8173519c72a6ee19ab10bcfe5bb4663a2c51a
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-20 11:12:11 +0200

    initial commit

--- src/main/scala/KafkaProducerApp.scala
+++ src/main/scala/KafkaProducerApp.scala
@@ -1,13 +1,15 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
 import java.util.Properties
 
 object KafkaProducerApp extends App {
 
   val props = new Properties()
 
-  props.put("bootstrap.servers", "localhost:9092")
-  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+  props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 
   val producer = new KafkaProducer[String, String](props)
   val i = for (i <- 0 until 100) {
